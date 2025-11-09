025cd8fa0c7a66185c15ac2a2443ccf4c6e8132e
saikumar001-sky <andili.saikumar@gmail.com>
2025-11-09 11:08:52 +0530

    testdev

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <div>Test code dummy component dev testclcls</div>
+    <div> component dev testclcls</div>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
